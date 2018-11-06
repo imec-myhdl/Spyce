f7dd0575b35972829e63028da7078851545f34a0
Paul Mateman <paul.mateman@imec-nl.nl>
2018-11-06 14:50:18 +0100

    added to easily (re)import a block

---
+++ BlockEditor/supsisim/src_import.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+utility to streamline the (re) importing of files
+"""
+
+import os, sys
+
+from supsisim.dialg import error
+
+def import_module_from_source_file(filepath):
+    fullpath       = os.path.abspath(filepath)
+    path, filename = os.path.split(fullpath)
+    modname, ext     = os.path.splitext(filename) # modname could be anything, but this seems cleanest
+
+    if sys.version_info >= (2,7) and sys.version_info < (3,0):
+        # python 2
+        try:
+            import imp
+            mod = imp.load_source(modname, fullpath)
+        except:
+            mod = None
+            error('loading of {} caused error:\n{}'.format(fullpath, sys.exc_info()[0]))
+
+    elif sys.version_info >= (3,3) and sys.version_info < (3,5):
+        # python 3.3 or 3.4
+        try:
+            from importlib.machinery import SourceFileLoader
+            mod = SourceFileLoader(modname, fullpath).load_module()
+        except:
+            mod = None
+            error('loading of {} caused error:\n{}'.format(fullpath, sys.exc_info()[0]))
+
+    elif sys.version_info >= (3,5):
+        # python 3.5+
+        try:
+            import importlib.util
+            spec = importlib.util.spec_from_file_location(modname, fullpath)
+            mod = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(mod)    
+        except:
+            mod = None
+            error('loading of {} caused error:\n{}'.format(fullpath, sys.exc_info()[0]))
+    else:
+        raise Exception ('This version of python is not supported\nUse latest 2.7, 3.3 or newer')
+
+    return mod
