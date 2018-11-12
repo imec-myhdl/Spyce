9acc2e1f0f503e384cb850984985dd06e074d603
Paul Mateman <paul.mateman@imec-nl.nl>
2018-11-12 11:47:51 +0100

    showcase parametrized block

--- BlockEditor/libraries/library_common/Sum.py
+++ BlockEditor/libraries/library_common/Sum.py
@@ -5,25 +5,69 @@
 inp = 2
 outp = 1
 
-parameters = dict(inp=2,outp=1)
-properties = {'name': 'sumBlk', 'Gains': ' [1,-1]'} #voor netlisten
+parameters = dict(A = '++')
+properties = {'name': 'sumBlk'} #voor netlisten
 #view variables:
-iconSource = 'SUM'
 
 
 
-import supsisim.block 
 
 def getSymbol(param,parent=None,scene=None,):
+    import supsisim.block
+    from  Qt import QtGui, QtWidgets, QtCore # see https://github.com/mottosso/Qt.py
+    gains = param['A'] if 'A' in param else '++'
     attributes = dict()
     attributes['name'] = name
     attributes['libname'] = libname
-    attributes['input'] = param['inp'] if 'inp' in param else inp
-    attributes['output'] = param['outp'] if 'outp' in param else outp
-    attributes['icon'] = iconSource
+    attributes['input'] = len(gains) if 'A' in param else inp
+    attributes['output'] = 1
+    b = supsisim.block.Block(attributes,param,properties, name, libname, parent, scene)
     
+    # create a new pathitem with a plus and citcle
+    pi = QtWidgets.QGraphicsPathItem(b)
+    pi.setPen(QtCore.Qt.black)
+    pp = QtGui.QPainterPath()
+    # draw plus
+    pp.moveTo(-11,0)
+    pp.lineTo(11, 0)
+    pp.moveTo(0, -11)
+    pp.lineTo(0, 11)
+    #draw circle
+    pp.addEllipse(-15, -15, 30, 30)
+    pi.setPath(pp)
+    pi.setPos(5,0) # move a little to the right for optical reasons
     
-    return supsisim.block.Block(attributes,param,properties,name,libname,parent,scene)
+    # add + or - signs to ports
+    for port in b.ports():
+        portname = port.label.text()
+        if portname.startswith('.i_'):
+            sign = gains[int(portname[3:])]
+            port.p.moveTo(10, 0)
+            port.p.lineTo(20, 0)
+            if sign == '+':
+                port.p.moveTo(15, -5)
+                port.p.lineTo(15, 5)
+            port.setPath(port.p)
+            
+    return b
     
+def toMyhdlInstance(name, connectdict, param):
+    # properties end up in the connectdict
+    gains = param['A'] if 'A' in param else '++' 
+    inputs = []
+    outp = connectdict['.o_0']
+    for ix, s in enumerate(gains):
+        net = connectdict['.i_{}'.format(ix)]
+        gain = gains[ix]
+        if s == '-':
+            inputs.append('- {}'.format(net))
+        else:
+            inputs.append('+ {}'.format(net))
+            inputs.append('+ {} * {}'.format(gain, net))
+    addition = '{}.next = {}'.format(outp, ' '.join(inputs).lstrip('+ '))
+    fmt = '    @always_comb\n' + \
+          '    def u_{}():\n' + \
+          '        {}\n'
+    return fmt.format(name, addition)
 
-views = {'icon':iconSource}
+views = {}
